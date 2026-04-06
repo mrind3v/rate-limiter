65f49828676a25c76535a29c036c708d6df99968
mrind3v <mrinmaydev@outlook.com>
2026-04-06 10:55:37 +0530

    wrote the fetchData() method inside the proxy, which first checks if an api request is valid or not using the strategy and then proceeds to reject that reqquest or let it hit the real resource

--- src/RateLimiterProxy.java
+++ src/RateLimiterProxy.java
@@ -1,10 +1,16 @@
 public class RateLimiterProxy implements ApiResource{
-    ApiResource apiResource;
-    public RateLimiterProxy(ApiResource apiResource) {
-        this.apiResource = apiResource;
+    private final ApiResource realApiResource;
+    private final RateLimitStrategy rateLimitStrategy;
+    public RateLimiterProxy(ApiResource apiResource, RateLimitStrategy rateLimitStrategy) {
+        this.realApiResource = apiResource;
+        this.rateLimitStrategy = rateLimitStrategy;
     }
     @Override
     public void fetchData() {
-
+        if (rateLimitStrategy.canRequest()) {
+            realApiResource.fetchData();
+        } else {
+            System.out.println("Api request rejected. Rate limit exceeded.");
+        }
     }
 }
